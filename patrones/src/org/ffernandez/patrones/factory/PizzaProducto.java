b9370eb61c72d24df7ad44ff9128e0d8043dadbf
PC <fernandezfrancoa2001@gmail.com>
2023-05-13 00:32:01 -0300

    patrones_abstract_factory

---
+++ patrones/src/org/ffernandez/patrones/factory/PizzaProducto.java
@@ -0,0 +1,40 @@
+package org.ffernandez.patrones.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PizzaProducto {
+    protected String nombre;
+    protected  String masa;
+    protected String salsa;
+    protected List<String> ingredientes;
+
+    public PizzaProducto() {
+        this.ingredientes = new ArrayList<>();
+    }
+
+    public void preparar(){
+        System.out.println("Preparando: " + nombre);
+        System.out.println("Seleccionando masa: " + masa);
+        System.out.println("Seleccionando salsa: " + salsa);
+        System.out.println("Agregando ingredientes: ");
+        this.ingredientes.forEach(System.out::println);
+    }
+
+
+    public abstract void hornear();
+    public void empacar(){
+        System.out.println("Empacando pizza");
+    }
+    public abstract void cortar();
+
+    @Override
+    public String toString() {
+        return "PizzaProducto{" +
+                "nombre='" + nombre + '\'' +
+                ", masa='" + masa + '\'' +
+                ", salsa='" + salsa + '\'' +
+                ", ingredientes=" + ingredientes +
+                '}';
+    }
+}
